6c79ac3372525887c653b8c0f4552f9ce84a4341
crgimenes <crg@crg.eti.br>
2023-07-10 21:54:29 -0300

    update package

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module realm
 go 1.20
 
 require (
+	crg.eti.br/go/config v1.4.13
+	github.com/dghubble/gologin/v2 v2.4.0
 	github.com/hajimehoshi/ebiten/v2 v2.5.5
 	github.com/jmoiron/sqlx v1.3.5
+	golang.org/x/oauth2 v0.10.0
 	modernc.org/sqlite v1.23.1
 	nhooyr.io/websocket v1.8.7
 )
@@ -13,20 +16,27 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/ebitengine/purego v0.3.2 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-github/v48 v48.2.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230626212559-97b1e661b5df // indirect
 	golang.org/x/image v0.9.0 // indirect
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
 	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/tools v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	lukechampine.com/uint128 v1.3.0 // indirect
 	modernc.org/cc/v3 v3.41.0 // indirect
 	modernc.org/ccgo/v3 v3.16.14 // indirect
--- server/main.go
+++ server/main.go
@@ -2,25 +2,208 @@ package main
 
 import (
 	"embed"
+	"encoding/json"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
 	"strings"
+	"text/template"
 	"time"
 
 	"realm/handler"
+	"realm/session"
+
+	"github.com/dghubble/gologin/v2"
+	"github.com/dghubble/gologin/v2/github"
+	"golang.org/x/oauth2"
+	githubOAuth2 "golang.org/x/oauth2/github"
+
+	"crg.eti.br/go/config"
+	_ "crg.eti.br/go/config/ini"
 )
 
-//go:embed assets/*
-var assets embed.FS
+type Config struct {
+	GithubClientID     string `ini:"github_client_id" cfg:"github_client_id" cfgRequired:"true" cfgHelper:"Github Client ID"`
+	GithubClientSecret string `ini:"github_client_secret" cfg:"github_client_secret" cfgRequired:"true" cfgHelper:"Github Client Secret"`
+	GithubCallbackURL  string `ini:"github_callback_url" cfg:"github_callback_url" cfgRequired:"true" cfgHelper:"Github Callback URL"`
+	DatabaseURL        string `ini:"database_url" cfg:"database_url" cfgRequired:"true" cfgHelper:"Database URL"`
+	Port               int    `ini:"port" cfg:"port" cfgDefault:"8080" cfgHelper:"Port"`
+}
+
+type sessionData struct {
+	OAuthProvider string
+	UserName      string
+	AvatarURL     string
+	SessionID     string
+}
+
+var (
+	sc *session.Control
+
+	//go:embed assets/*
+	assets embed.FS
+)
+
+// ///////////////////////////////////
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("homeHandler")
+
+	sid, sd, ok := sc.Get(r)
+	if !ok {
+		fmt.Println("1 session not found")
+		sid, sd = sc.Create()
+	}
+
+	if sd.Data != nil {
+		sdAUX, ok := sd.Data.(*sessionData)
+		if !ok {
+			log.Fatal("type assertion failed sessionData")
+		}
+		fmt.Println("name:", sdAUX.UserName)
+	} else {
+		fmt.Println("sd.Data is nil")
+	}
+
+	// renew session
+	sc.Save(w, sid, sd)
+
+	fmt.Println("sid:", sid)
+	//////////////////////////
+
+	index, err := assets.ReadFile("assets/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, err := template.New("index.html").Parse(string(index))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		sdAUX *sessionData
+	)
+
+	if sd.Data != nil {
+		sdAUX, ok = sd.Data.(*sessionData)
+		if !ok {
+			log.Fatal("type assertion failed sessionData")
+		}
+	}
+	data := struct {
+		SessionData    *sessionData
+		GitHubLoginURL string
+		LogoutURL      string
+	}{
+		SessionData:    sdAUX,
+		GitHubLoginURL: "/forum/github/login",
+		LogoutURL:      "/forum/logout",
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("logoutHandler")
+	sid, sd, ok := sc.Get(r)
+	if !ok {
+		http.Redirect(w, r, "/forum", http.StatusFound)
+		return
+	}
+
+	sd.Data = nil
+
+	// remove session
+	sc.Delete(w, sid)
+
+	http.Redirect(w, r, "/forum", http.StatusFound)
+}
+
+func issueSession() http.Handler {
+	fmt.Println("issueSession")
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		sid, sd, ok := sc.Get(r)
+		if !ok {
+			fmt.Println("2 session not found")
+			sid, sd = sc.Create()
+		}
+
+		ctx := r.Context()
+		githubUser, err := github.UserFromContext(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("sid:", sid)
+		fmt.Println("githubUser id.........:", *githubUser.ID)
+		fmt.Println("githubUser login......:", *githubUser.Login)
+		fmt.Println("githubUser email......:", *githubUser.Email)
+		fmt.Println("githubUser name.......:", *githubUser.Name)
+		fmt.Println("githubUser avatar.....:", *githubUser.AvatarURL)
+		fmt.Println("githubUser url........:", *githubUser.URL)
+		fmt.Println("githubUser html url...:", *githubUser.HTMLURL)
+		fmt.Println("githubUser followers..:", *githubUser.Followers)
+		fmt.Println("githubUser following..:", *githubUser.Following)
+		fmt.Println("githubUser created at.:", *githubUser.CreatedAt)
+
+		sdAUX := sessionData{
+			OAuthProvider: "github",
+			UserName:      *githubUser.Name,
+			AvatarURL:     *githubUser.AvatarURL,
+			SessionID:     sid,
+		}
+		sd.Data = &sdAUX
+
+		fmt.Println("name:", sdAUX.UserName)
+		// renew session
+		sc.Save(w, sid, sd)
+
+		http.Redirect(w, r, "/forum", http.StatusFound)
+	}
+	return http.HandlerFunc(fn)
+}
+
+/////////////////////////////////////
 
 func main() {
+	cfg := Config{}
+
+	config.File = "config.ini"
+	err := config.Parse(&cfg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	const cookieName = "forum_session"
+	sc = session.New(cookieName)
+
+	go func() {
+		for {
+			time.Sleep(5 * time.Minute)
+			sc.RemoveExpired()
+		}
+	}()
+
+	oauth2Config := &oauth2.Config{
+		ClientID:     cfg.GithubClientID,
+		ClientSecret: cfg.GithubClientSecret,
+		RedirectURL:  cfg.GithubCallbackURL,
+		Endpoint:     githubOAuth2.Endpoint,
+	}
+
+	// state param cookies require HTTPS by default; disable for localhost development
+	stateConfig := gologin.DebugOnlyCookieConfig
 
 	assetsRFS, _ := fs.Sub(assets, "assets")
 	var assetsFS = http.FS(assetsRFS)
 
 	fs := http.FileServer(assetsFS)
-	log.Print("Serving on :8888")
 
 	mux := http.NewServeMux()
 
@@ -35,13 +218,57 @@ func main() {
 	mux.HandleFunc("/ws", handler.Websocket)
 	mux.HandleFunc("/forum/", handler.Forum)
 
+	mux.HandleFunc("/", homeHandler)
+	//mux.HandleFunc("/login", loginHandler)
+	mux.HandleFunc("/logout", logoutHandler)
+
+	mux.Handle(
+		"/github/login",
+		github.StateHandler(
+			stateConfig,
+			github.LoginHandler(oauth2Config, nil)))
+	mux.Handle(
+		"/github/callback",
+		github.StateHandler(
+			stateConfig,
+			github.CallbackHandler(oauth2Config, issueSession(), nil)))
+
+	// recebe post de usuário
+	mux.HandleFunc("/forum/post", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("postHandler")
+		sid, sd, ok := sc.Get(r)
+		if !ok {
+			http.Redirect(w, r, "/forum", http.StatusFound)
+			return
+		}
+
+		sdAUX, ok := sd.Data.(*sessionData)
+		if !ok {
+			log.Fatal("type assertion failed sessionData")
+		}
+		fmt.Println("name:", sdAUX.UserName)
+
+		var post string
+		err := json.NewDecoder(r.Body).Decode(&post)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("sid:", sid)
+		fmt.Println("post:", post)
+	})
+
 	s := &http.Server{
 		Handler:        mux,
-		Addr:           ":8888",
+		Addr:           fmt.Sprintf(":%d", cfg.Port),
 		ReadTimeout:    5 * time.Second,
 		WriteTimeout:   5 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
+	println(cfg.DatabaseURL)
+
+	log.Printf("Listening on port %d\n", cfg.Port)
 	log.Fatal(s.ListenAndServe())
 }
