2350311d42cf0aae53125426724aa4efd4b9816a
crgimenes <crg@crg.eti.br>
2023-07-11 23:19:24 -0300

    add sql

--- db/sqlite/sqlite.go
+++ db/sqlite/sqlite.go
@@ -37,17 +37,49 @@ func (s *Sqlite) Close() error {
 }
 
 func (s *Sqlite) CreateTables() error {
-	return nil
+	sqlStatement := `
+	create table if not exists session (
+		session_id text primary key,
+		expire_at integer not null,
+		data text not null
+	);`
+
+	_, err := s.DB.Exec(sqlStatement)
+
+	return err
 }
 
 func (s *Sqlite) SaveSession(sessionID string, sd *session.SessionData) error {
-	return nil
+	// insert ou update
+	sqlStatement := `
+	insert into session (
+		session_id, 
+		expire_at, 
+		data) 
+	values ($1, $2, $3)
+	on conflict(session_id) 
+	do update set expire_at = $2, data = $3;`
+
+	_, err := s.DB.Exec(sqlStatement, sessionID, sd.ExpireAt, sd.Data)
+
+	return err
 }
 
 func (s *Sqlite) DeleteSession(sessionID string) error {
-	return nil
+	sqlStatement := `
+	delete from session where session_id = $1;`
+
+	_, err := s.DB.Exec(sqlStatement, sessionID)
+
+	return err
 }
 
 func (s *Sqlite) GetSession(sessionID string) (*session.SessionData, error) {
-	return nil, nil
+	sqlStatement := `
+	select data from session where session_id = $1;`
+
+	var data session.SessionData
+	err := s.DB.Get(&data, sqlStatement, sessionID)
+
+	return &data, err
 }
